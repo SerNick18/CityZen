60c64629412a1187bdc5aad52f2d4ec4b453f583
SerNick18 <nicola.serra@hotmail.it>
2020-12-17 11:59:16 +0100

    Aggiunto uno spazio in LoginServlet

--- src/main/java/controller/gestioneUtenza/LoginServlet.java
+++ src/main/java/controller/gestioneUtenza/LoginServlet.java
@@ -14,6 +14,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req,resp);
+
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
