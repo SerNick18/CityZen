048ac476a5d591bf5eac39047a786041130eb476
DESKTOP-VGPC932\Luke <lucas.memoli@gmail.com>
2020-12-18 17:13:50 +0100

    aggiuto FacadeTest

---
+++ src/test/java/model/gestioneDati/facadeDataAccess/FacadeDAOTest.java
@@ -0,0 +1,18 @@
+package model.gestioneDati.facadeDataAccess;
+
+import model.gestioneDati.modelDataAccess.CittadinoDAO;
+import model.gestioneDati.modelObjects.Cittadino;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacadeDAOTest extends FacadeDAO {
+    @Test
+    public void controlLogin()
+    {
+        FacadeDAO f = new FacadeDAO();
+        Cittadino c = f.login("pippo@alice.it", "1111111");
+        assertEquals(new Cittadino(),c);
+    }
+
+}
