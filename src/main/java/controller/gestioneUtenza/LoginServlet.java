d1f189bb70c38e9afdc90547d76fbcb559d6ed06
Antonio-28 <antoniorusso230@gmail.com>
2020-12-18 19:39:17 +0100

    aggiunta funzionalità di login

--- src/main/java/controller/gestioneUtenza/LoginServlet.java
+++ src/main/java/controller/gestioneUtenza/LoginServlet.java
@@ -1,13 +1,17 @@
 package controller.gestioneUtenza;
 
+import model.gestioneDati.facadeDataAccess.FacadeDAO;
 import model.gestioneDati.modelObjects.Cittadino;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -19,9 +23,24 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
-        String pwd = req.getParameter("pwd1");
+        String pwd = req.getParameter("password");
 
-        Cittadino c = new Cittadino();
+        if(email.compareTo("")==0 || !Pattern.matches("[A-Za-z.]+[0-9]*@[A-Za-z.]+", email) || pwd.compareTo("")==0){
+            //errore
+        }else{
+            FacadeDAO service=new FacadeDAO();
+            Cittadino c = service.login(email, pwd);
+
+            HttpSession sn=req.getSession();
+
+            if(c!=null){
+                sn.setAttribute("Utente", c);
+                RequestDispatcher dispatcher=req.getRequestDispatcher("/index.jsp");
+                dispatcher.forward(req, resp);
+            }else{
+                //email o password sbagliate
+            }
+        }
 
     }
 }
