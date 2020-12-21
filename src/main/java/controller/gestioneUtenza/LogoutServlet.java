dac28ed878b620682cc53a9168dc69802946ba58
p.napolitano18@studenti.unisa.it <Quadrelle1>
2020-12-21 20:59:19 +0100

    javadoc LogoutServlet

--- src/main/java/controller/gestioneUtenza/LogoutServlet.java
+++ src/main/java/controller/gestioneUtenza/LogoutServlet.java
@@ -6,7 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Servlet per il logout
+ */
 public class LogoutServlet extends HttpServlet {
+    /**
+     * Metodo per invalidare la sessione
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().invalidate();
