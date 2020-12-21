cbb4a5ac31cb9359b52b3f71d412d4d5971cd504
FraSab98 <67547359+FraSab98@users.noreply.github.com>
2020-12-21 17:58:26 +0100

    completamento javadoc per la registrazione e aggiunti ulteriori controlli

--- src/main/java/controller/gestioneUtenza/MyRuntimeException.java
+++ src/main/java/controller/gestioneUtenza/MyRuntimeException.java
@@ -1,21 +1,19 @@
 package controller.gestioneUtenza;
-
+/**
+ * Questa servlet server per gestire i messaggi di errore
+ * lanciati dal nostro sistema.
+ * */
 public class MyRuntimeException extends RuntimeException{
-    public MyRuntimeException() {
-    }
-
+    public MyRuntimeException() { }
     public MyRuntimeException(String message) {
         super(message);
     }
-
     public MyRuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
-
     public MyRuntimeException(Throwable cause) {
         super(cause);
     }
-
     public MyRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
