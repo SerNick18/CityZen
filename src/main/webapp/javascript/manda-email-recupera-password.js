b14aaf28b9d95b15195373e8760cac1a6bf616fc
Antonio-28 <antoniorusso230@gmail.com>
2020-12-21 18:44:56 +0100

    aggiunti controlli lato client per l'email nel form di recupera-password

--- src/main/webapp/javascript/manda-email-recupera-password.js
+++ src/main/webapp/javascript/manda-email-recupera-password.js
@@ -1,6 +1,7 @@
 $("document").ready(function(){
     $(".alert").hide();
     $("#loading").hide();
+    $(".btn-success").attr("disabled", true);
 
     $("#form-recupera").submit(function(e){
         e.preventDefault();
@@ -16,11 +17,14 @@ $("document").ready(function(){
                 $(".alert").hide();
                 if(data.includes("ok")){
                     $("#email").val("");
+                    //ripristina il colore iniziale del bordo
+                    $("#email").css("border-color", "#ced4da");
                     $(".alert-success").show();
                 }else if(data.includes("errore")){
                     $(".alert-danger").text("Non siamo riusciti ad inviarti una mail").show();
                 }else if(data.includes("non-registrato")){
                     $(".alert-danger").text("L'indirizzo e-mail non risulta presente").show();
+                    $("#email").css("border-color", "red");
                 }
             },
             error: function () {
@@ -30,5 +34,20 @@ $("document").ready(function(){
             }
         });
 
+    })
+
+    //controllo pattern email
+    var email=$("#email");
+    var emailPattern=new RegExp("[A-Za-z.]+[0-9]*@[A-Za-z.]+");
+    $("#email").keyup(function (){
+        if(!emailPattern.test(email.val())){
+            //imposta il colore del bordo a rosso
+            email.css("border-color", "red");
+            $(".btn-success").attr("disabled", true);
+        }else{
+            //imposta il colore del bordo a verde
+            email.css("border-color", "green");
+            $(".btn-success").attr("disabled", false);
+        }
     })
 })
