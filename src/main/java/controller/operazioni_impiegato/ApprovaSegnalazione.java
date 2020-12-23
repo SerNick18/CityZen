b7991ebc92550a4a205c27412b486a3cab495852
p.napolitano18@studenti.unisa.it <Quadrelle1>
2020-12-23 15:32:46 +0100

    correzione checkstyle

--- src/main/java/controller/operazioni_impiegato/ApprovaSegnalazione.java
+++ src/main/java/controller/operazioni_impiegato/ApprovaSegnalazione.java
@@ -12,31 +12,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.SQLException;
+
+/**.
+ * Servlet per approvare una segnalazione
+ */
 @WebServlet("/approva")
 public class ApprovaSegnalazione extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    /**.
+     * Metodo per approvare una segnalazione
+     * @param req request
+     * @param resp response
+     * @throws ServletException Eccezione
+     * @throws IOException Eccezione
+     */
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         HttpSession session = req.getSession();
         Impiegato impiegato = (Impiegato) session.getAttribute("Impiegato");
-        if(impiegato == null)
-            throw new MyServletException("Effettua il login per poter effettuare" +
-                    " questa operazione");
+        if (impiegato == null) {
+            throw new MyServletException(
+                    "Effettua il login per poter effettuare"
+                    + " questa operazione");
+        }
         FacadeDAO service = new FacadeDAO();
-        if(req.getParameter("id") != null) {
-            Segnalazione segnalazione = new FacadeDAO().getSegnalazioneById(Integer.parseInt(req.getParameter("id")));
-            if(segnalazione != null && segnalazione.getStato().equals("inoltrata")) {
+        if (req.getParameter("id") != null) {
+            Segnalazione segnalazione =
+                    new FacadeDAO().getSegnalazioneById(
+                            Integer.parseInt(req.getParameter("id")));
+            if (segnalazione != null
+                    && segnalazione.getStato().equals("inoltrata")) {
                 segnalazione.setStato("approvata");
                 service.modificaSegnalazione(segnalazione);
                 service.inserisciLavorazione(impiegato, segnalazione);
-                req.getRequestDispatcher("gui-impiegato.jsp").forward(req, resp);
+                req.getRequestDispatcher(
+                        "/WEB-INF/view/GuiImpiegato/gui-impiegato.jsp")
+                        .forward(req, resp);
             } else {
-                throw new MyServletException("Indicare una segnalazione correttamente");
+                throw new MyServletException(
+                        "Indicare una segnalazione correttamente");
             }
         } else {
             throw new MyServletException("Indicare una segnalazione");
         }
     }
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    /**.
+     * Metodo doPost che richiama semplicemente il doGet
+     * @param req request
+     * @param resp response
+     * @throws ServletException eccezione
+     * @throws IOException eccezione
+     */
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         doGet(req, resp);
     }
 }
--- src/main/java/model/gestioneDati/modelDataAccess/SegnalazioneDAO.java
+++ src/main/java/model/gestioneDati/modelDataAccess/SegnalazioneDAO.java
@@ -2,29 +2,41 @@ package model.gestioneDati.modelDataAccess;
 
 import controller.gestioneUtenza.MyRuntimeException;
 import model.gestioneDati.facadeDataAccess.FacadeDAO;
-import model.gestioneDati.modelObjects.Cittadino;
 import model.gestioneDati.modelObjects.Segnalazione;
 import model.gestioneDati.modelObjects.SegnalazioneInterface;
 import model.gestioneDati.modelObjects.SegnalazioneProxy;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SegnalazioneDAO {
+    /**.
+     * Metodo che ritorna la lista di SegnalazioneProxy inoltrate
+     * @param offset numero di segnalazioni da mostrare
+     * @return lista di SegnalazioneProxy inoltrate
+     */
     public List<SegnalazioneInterface> doRetrieveInoltrateProxy(int offset) {
         try {
             ArrayList<SegnalazioneInterface> segnalazioni = new ArrayList<>();
             FacadeDAO facadeDAO = new FacadeDAO();
             Connection connection = ConnectionPool.getConnection();
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT * FROM segnalazione WHERE stato=? order by id desc limit 6 offset ?");
+                    "SELECT * FROM segnalazione WHERE stato=? "
+                            + "order by id desc limit 6 offset ?");
             statement.setString(1, "inoltrata");
             statement.setInt(2, offset);
             ResultSet r = statement.executeQuery();
             while (r.next()) {
-                SegnalazioneProxy s = new SegnalazioneProxy(r.getInt("ID"),r.getString("Oggetto"),
-                        r.getInt("Priorità"), facadeDAO.getCittadinoByCf(r.getString("Cittadino")), r.getInt("numSolleciti"));
+                SegnalazioneProxy s = new SegnalazioneProxy(
+                        r.getInt("ID"), r.getString("Oggetto"),
+                        r.getInt("Priorità"),
+                        facadeDAO.getCittadinoByCf(r.getString("Cittadino")),
+                        r.getInt("numSolleciti"));
                 segnalazioni.add(s);
             }
             return segnalazioni;
@@ -33,19 +45,30 @@ public class SegnalazioneDAO {
         }
     }
 
-    public List<SegnalazioneInterface> doRetrieveByStato(String stato, int offset) {
+    /**.
+     * Metodo che ritorna la lista delle segnalazioni per stato
+     * @param stato stato della segnalazione
+     * @param offset numero di segnalazioni da mostrare
+     * @return offset segnalazioni per stato
+     */
+    public List<SegnalazioneInterface> doRetrieveByStato(
+            String stato, int offset) {
         try {
             ArrayList<SegnalazioneInterface> segnalazioni = new ArrayList<>();
             FacadeDAO facadeDAO = new FacadeDAO();
             Connection connection = ConnectionPool.getConnection();
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT * FROM segnalazione WHERE stato=? order by id desc limit 20 offset ?");
+                    "SELECT * FROM segnalazione WHERE stato=? "
+                            + "order by id desc limit 20 offset ?");
             statement.setString(1, stato);
             statement.setInt(2, offset);
             ResultSet r = statement.executeQuery();
             while (r.next()) {
-                SegnalazioneProxy s = new SegnalazioneProxy(r.getInt("ID"),r.getString("Oggetto"),
-                        r.getInt("Priorità"), facadeDAO.getCittadinoByCf(r.getString("Cittadino")), r.getInt("numSolleciti"));
+                SegnalazioneProxy s = new SegnalazioneProxy(
+                        r.getInt("ID"), r.getString("Oggetto"),
+                        r.getInt("Priorità"),
+                        facadeDAO.getCittadinoByCf(r.getString("Cittadino")),
+                        r.getInt("numSolleciti"));
                 segnalazioni.add(s);
             }
             return segnalazioni;
@@ -53,8 +76,11 @@ public class SegnalazioneDAO {
             throw new RuntimeException(e);
         }
     }
-
-
+    /**.
+     * Metodo per ricercare una segnalazione dato un id
+     * @param id identificativo della segnalazione
+     * @return segnalazione con l'id passato come parametro
+     */
     public Segnalazione doRetrieveById(int id) {
         try {
             FacadeDAO facadeDAO = new FacadeDAO();
@@ -64,10 +90,15 @@ public class SegnalazioneDAO {
             statement.setInt(1, id);
             ResultSet r = statement.executeQuery();
             if (r.next()) {
-                Segnalazione s = new Segnalazione(r.getInt("ID"),r.getString("Via"),
-                        r.getInt("Civico"), r.getInt("Priorità"), r.getInt("numSolleciti"),
-                        r.getString("Stato"), r.getDate("DataSegnalazione"), r.getString("Oggetto"),
-                        r.getString("Descrizione"), r.getString("Foto"),
+                Segnalazione s = new Segnalazione(
+                        r.getInt("ID"), r.getString("Via"),
+                        r.getInt("Civico"), r.getInt("Priorità"),
+                        r.getInt("numSolleciti"),
+                        r.getString("Stato"),
+                        r.getDate("DataSegnalazione"),
+                        r.getString("Oggetto"),
+                        r.getString("Descrizione"),
+                        r.getString("Foto"),
                         facadeDAO.getCittadinoByCf(r.getString("Cittadino")),
                         r.getInt("Riaperta"));
                 return s;
@@ -77,23 +108,28 @@ public class SegnalazioneDAO {
             throw new RuntimeException(e);
         }
     }
-
-    public void doInsert(Segnalazione segnalazione){
+    /**.
+     * Metodo per inserire una segnalazione
+     * @param segnalazione segnalazione da inserire
+     */
+    public void doInsert(Segnalazione segnalazione) {
         try {
             Connection connection = ConnectionPool.getConnection();
-            PreparedStatement statement = connection.prepareStatement("insert into segnalazione" +
-                    "(Via, Civico, Priorità, numSolleciti, Stato, DataSegnalazione, Oggetto, Descrizione, Foto, Cittadino) "
-                    +"values(?,?,?,?,?,?,?,?,?,?)");
-            statement.setString(1,segnalazione.getVia());
-            statement.setInt(2,segnalazione.getCivico());
-            statement.setInt(3,segnalazione.getPriorita());
-            statement.setInt(4,segnalazione.getNumSolleciti());
-            statement.setString(5,segnalazione.getStato());
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into segnalazione (Via, Civico, Priorità,"
+                            + "numSolleciti, Stato, DataSegnalazione, Oggetto,"
+                            + "Descrizione, Foto, Cittadino)"
+                            + "values(?,?,?,?,?,?,?,?,?,?)");
+            statement.setString(1, segnalazione.getVia());
+            statement.setInt(2, segnalazione.getCivico());
+            statement.setInt(3, segnalazione.getPriorita());
+            statement.setInt(4, segnalazione.getNumSolleciti());
+            statement.setString(5, segnalazione.getStato());
             statement.setDate(6, new java.sql.Date(segnalazione.getDataSegnalazione().getTime()));
-            statement.setString(7,segnalazione.getOggetto());
-            statement.setString(8,segnalazione.getDescrizione());
-            statement.setString(9,segnalazione.getFoto());
-            statement.setString(10,segnalazione.getCittadino().getCF());
+            statement.setString(7, segnalazione.getOggetto());
+            statement.setString(8, segnalazione.getDescrizione());
+            statement.setString(9, segnalazione.getFoto());
+            statement.setString(10, segnalazione.getCittadino().getCF());
             if(statement.executeUpdate()!=1)
                 throw new MyRuntimeException("C'è stato un errore nell'inserimento della segnalazione");
         } catch (SQLException e) {
