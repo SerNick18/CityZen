9cd917b56fa68a2326ee911b9d51f3881e72390a
FraSab98 <67547359+FraSab98@users.noreply.github.com>
2020-12-23 13:28:59 +0100

    aggiunti javadoc per VisualizzaDettagliSegnalazioneServlet

--- src/main/java/controller/gestioneUtenza/VisualizzaDettagliSegnalazioneServlet.java
+++ src/main/java/controller/gestioneUtenza/VisualizzaDettagliSegnalazioneServlet.java
@@ -10,7 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+/**
+ *
+ * Questa servlet prende una segnalazione e mostra i dettagli,
+ * sia al cittadino
+ * sia all'impiegato.
+ * */
 @WebServlet("/dettagli")
 public class VisualizzaDettagliSegnalazioneServlet extends HttpServlet {
     /**
@@ -24,28 +29,38 @@ public class VisualizzaDettagliSegnalazioneServlet extends HttpServlet {
             throws ServletException, IOException {
         doPost(req, resp);
     }
+    /**
+     * Metodo per poter visualizza i dettagli della segnalazione.
+     * Riceve l'id della segnalazione.
+     * @param req request
+     * @param resp response
+     * @throws ServletException se la richiesta non può essere gestita
+     * @throws IOException se viene rilevato un errore di input o output
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
         FacadeDAO service = new FacadeDAO();
         int id;
-
         try {
             id = Integer.parseInt(req.getParameter("id"));
-
         } catch (NumberFormatException e) {
             throw new MyServletException("segnalazione non valida");
         }
         Segnalazione segnalazione = service.getSegnalazioneById(id);
+        if (segnalazione == null){
+            throw new MyServletException("Nessuna segnalazione trovata");
+        }
         req.getSession().setAttribute("Segnalazione", segnalazione);
         if(req.getSession().getAttribute("Cittadino") != null){
             RequestDispatcher dispatcher =
-                req.getRequestDispatcher("/WEB-INF/view/GuiCittadino/dettagli-segnalazione.jsp");
+                req.getRequestDispatcher(
+                        "/WEB-INF/view/GuiCittadino/dettagli-segnalazione.jsp");
             dispatcher.forward(req, resp);
         }else if(req.getSession().getAttribute("Impiegato") != null) {
             RequestDispatcher dispatcher =
-                    req.getRequestDispatcher("/WEB-INF/view/GuiImpiegato/dettagli-segnalazione.jsp");
+                    req.getRequestDispatcher("" +
+                            "/WEB-INF/view/GuiImpiegato/dettagli-segnalazione.jsp");
             dispatcher.forward(req, resp);
         }else {
             throw new MyServletException("Impossibile vedere i dettagli se non si è autenticato");
