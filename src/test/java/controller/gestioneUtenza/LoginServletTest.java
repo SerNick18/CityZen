bc452bd928e6ab4d6cfe28c274542f6bdfc7eb2d
aless <a.fiordellisi3@studenti.unisa.it>
2020-12-30 15:09:30 +0100

    aggiunti test al LoginServletTest

--- src/test/java/controller/gestioneUtenza/LoginServletTest.java
+++ src/test/java/controller/gestioneUtenza/LoginServletTest.java
@@ -19,25 +19,33 @@ class LoginServletTest extends LoginServlet {
     }
 
     @Test
-    void testParamNull() {
-        request.addParameter("email", "");
-        request.addParameter("pwd", "");
-        MyServletException exception = assertThrows(MyServletException.class, () -> {loginServlet.doPost(request, response);});
-        assertEquals("Controlla correttezza campi", exception.getMessage());
-    }
-
-    @Test
     void testEmailNull(){
         request.addParameter("email", "");
-        request.addParameter("pwd", "Prova123");
+        request.addParameter("pwd", "Password00");
         MyServletException exception = assertThrows(MyServletException.class, () -> {loginServlet.doPost(request, response);});
         assertEquals("Controlla correttezza campi", exception.getMessage());
     }
 
     @Test
-    void testPwdNull(){
-        request.addParameter("email", "prova@gmail.com");
-        request.addParameter("pwd", "");
+    void testEmailErrata(){
+        request.addParameter("email", "alessioscafatiit");
+        request.addParameter("pwd", "Password00");
+        MyServletException exception = assertThrows(MyServletException.class, () -> {loginServlet.doPost(request, response);});
+        assertEquals("Controlla correttezza campi", exception.getMessage());
+    }
+
+    @Test
+    void testPwdLunghezzaErrata(){
+        request.addParameter("email", "alessio@scafati.it");
+        request.addParameter("pwd", "passw");
+        MyServletException exception = assertThrows(MyServletException.class, () -> {loginServlet.doPost(request, response);});
+        assertEquals("Controlla correttezza campi", exception.getMessage());
+    }
+
+    @Test
+    void testPwdFormatoErrato(){
+        request.addParameter("email", "alessio@scafati.it");
+        request.addParameter("pwd", "password");
         MyServletException exception = assertThrows(MyServletException.class, () -> {loginServlet.doPost(request, response);});
         assertEquals("Controlla correttezza campi", exception.getMessage());
     }
@@ -58,19 +66,5 @@ class LoginServletTest extends LoginServlet {
         assertEquals("Email o password errati", exception.getMessage());
     }
 
-    @Test
-    void testConImpiegato(){
-        //per passare il test ci deve stare questo record nel db
-        request.addParameter("email", "ugo@scafati.it");
-        request.addParameter("pwd", "Prova123");
-        assertDoesNotThrow(() -> {loginServlet.doPost(request, response);});
-    }
 
-    @Test
-    void testConCittadino(){
-        //per passare il test ci deve stare questo record nel db
-        request.addParameter("email", "giuseppe@gmail.com");
-        request.addParameter("pwd", "Prova123");
-        assertDoesNotThrow(() -> {loginServlet.doPost(request, response);});
-    }
 }
