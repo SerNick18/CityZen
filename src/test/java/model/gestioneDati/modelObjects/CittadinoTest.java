a89ac211eed49037916becc7a5df89e3adc807b1
p.napolitano18@studenti.unisa.it <Quadrelle1>
2021-01-13 16:45:37 +0100

    junit cittadino bean 100% coverage branch

--- src/test/java/model/gestioneDati/modelObjects/CittadinoTest.java
+++ src/test/java/model/gestioneDati/modelObjects/CittadinoTest.java
@@ -7,81 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CittadinoTest {
     private Cittadino cittadino = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
             "via roma",3,"Fisciano","cattaneo@gmail.com",0,0);
-    @Test
-    void testCF() {
-        cittadino.setCF("NPLPQL97B13A509L");
-            assertEquals("NPLPQL97B13A509L", cittadino.getCF());
-    }
-
-    @Test
-    void testNome() {
-        cittadino.setNome("Pasquale");
-        assertEquals("Pasquale", cittadino.getNome());
-    }
-
-    @Test
-    void testCognome() {
-        cittadino.setCognome("Napolitano");
-        assertEquals("Napolitano", cittadino.getCognome());
-    }
-
-    @Test
-    void testPwd() {
-        cittadino.setPwd("Pippotto8.");
-        assertEquals("Pippotto8.", cittadino.getPwd());
-    }
-
-    @Test
-    void testVia() {
-        cittadino.setVia("autostrada");
-        assertEquals("autostrada", cittadino.getVia());
-    }
-
-    @Test
-    void testCvicio() {
-        cittadino.setCivico(23);
-        assertEquals(23, cittadino.getCivico());
-    }
-
-    @Test
-    void testCitta() {
-        cittadino.setCitta("Bologna");
-        assertEquals("Bologna", cittadino.getCitta());
-    }
-
-    @Test
-    void testEmail() {
-        cittadino.setEmail("asd@gmail.com");
-        assertEquals("asd@gmail.com", cittadino.getEmail());
-    }
-
-    @Test
-    void testNumSegnalazioni() {
-        cittadino.setNumSegnalazioni(40);
-        assertEquals(40, cittadino.getNumSegnalazioni());
-    }
-
-    @Test
-    void testNumSegnAppr() {
-        cittadino.setNumSegnApp(10);
-        assertEquals(10, cittadino.getNumSegnApp());
-    }
-
-    @Test
-    void testToString() {
-        assertEquals("Cittadino{"
-                + "email='" + cittadino.getEmail() + '\''
-                + ", pwd='" + cittadino.getPwd() + '\''
-                + ", CF='" + cittadino.getCF() + '\''
-                + ", nome='" + cittadino.getNome() + '\''
-                + ", cognome='" + cittadino.getCognome() + '\''
-                + ", via='" + cittadino.getVia() + '\''
-                + ", civico='" + cittadino.getCivico() + '\''
-                + ", città='" + cittadino.getCitta() + '\''
-                + ", numSegnalazioni=" + cittadino.getNumSegnalazioni()
-                + ", numSegnApp=" + cittadino.getNumSegnApp()
-                + '}', cittadino.toString());
-    }
 
     @Test
     void testEqualsTrue() {
@@ -102,6 +27,69 @@ public class CittadinoTest {
     void testEqualsFalse3() {
         Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
                 "via roma",3,"Baronissi","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse4() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489S", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse5() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Pasquale", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse6() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Napolitano", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse7() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "Pippotto8.",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse8() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via gramsci",3,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse9() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",6,"Fisciano","cattaneo@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse10() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","catt@gmail.com",0,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse11() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",1,0);
+        assertEquals(false, cittadino.equals(cittadino2));
+    }
+
+    @Test
+    void testEqualsFalse12() {
+        Cittadino cittadino2 = new Cittadino("CPNLLD11S19A489D", "Giuseppe", "Cattaneo", "32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573",
+                "via roma",3,"Fisciano","cattaneo@gmail.com",0,1);
         assertEquals(false, cittadino.equals(cittadino2));
     }
 }
