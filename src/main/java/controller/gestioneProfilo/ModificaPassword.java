d366b33d5c678a762400f09ae1275a4c818be2ca
leo-capvano <leocapuano0@gmail.com>
2021-01-13 18:39:09 +0100

    - fix regex ModificaPassword

--- src/main/java/controller/gestioneProfilo/ModificaPassword.java
+++ src/main/java/controller/gestioneProfilo/ModificaPassword.java
@@ -85,8 +85,7 @@ public class ModificaPassword extends HttpServlet {
                         throw new MyServletException("Le due password"
                                 + " nuove non rispettano il formato ("
                                 + "Almeno 8 caratteri, 1 lettera maiuscola,"
-                                + " 1 minuscola, 1 numero"
-                                + " ed 1 carattere speciale");
+                                + " 1 minuscola, 1 numero");
                     }
                     if (!newPass.equals(newPass2)) {
                         throw new MyServletException("Le password nuove "
