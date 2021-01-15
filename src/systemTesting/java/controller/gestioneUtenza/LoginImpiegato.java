2ad0e93bab1a948e87b88d9c16d8fcf48900c2ef
FraSab98 <67547359+FraSab98@users.noreply.github.com>
2021-01-15 23:14:13 +0100

    testing di sistema

---
+++ src/systemTesting/java/controller/gestioneUtenza/LoginImpiegato.java
@@ -0,0 +1,81 @@
+package controller.gestioneUtenza;
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LoginImpiegato {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\Francesco Sabia\\Downloads\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testLoginImpiegato() throws Exception {
+        driver.get("http://localhost:8080/CityZen_war_exploded/index.jsp");
+        driver.findElement(By.linkText("Accedi")).click();
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("francesco@scafati.it");
+        driver.findElement(By.id("pwd")).clear();
+        driver.findElement(By.id("pwd")).sendKeys("Password1");
+        driver.findElement(By.id("loginAccesso")).submit();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
+
