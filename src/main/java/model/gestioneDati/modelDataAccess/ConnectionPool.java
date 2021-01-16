83dd9385769d88eb3f1420872d4698fda3591457
SerNick18 <nicola.serra@hotmail.it>
2021-01-16 13:11:54 +0100

    modifica file ConnectionPool

--- src/main/java/model/gestioneDati/modelDataAccess/ConnectionPool.java
+++ src/main/java/model/gestioneDati/modelDataAccess/ConnectionPool.java
@@ -28,7 +28,8 @@ public class ConnectionPool {
                     + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
             p.setUsername("root");
-            p.setPassword("admin");
+           // p.setPassword("admin");
+            p.setPassword("");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
