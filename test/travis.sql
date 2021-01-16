c392297eab9c52a0f15de575dfc68ea931f47f61
SerNick18 <nicola.serra@hotmail.it>
2021-01-16 13:41:43 +0100

    modifica file travis

--- test/travis.sql
+++ test/travis.sql
@@ -1,7 +1,4 @@
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'admin';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'root'@'localhost'
-
 CREATE DATABASE  IF NOT EXISTS `cityzendb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `cityzendb`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
