93239e40f883dccf5b806aa1f3ba684515a4120b
Antonio-28 <antoniorusso230@gmail.com>
2021-01-18 11:10:20 +0100

    Creata cartella test bean

---
+++ src/test/java/model/gestioneDati/modelObjects/CittadinoTest.java
@@ -0,0 +1,12 @@
+package model.gestioneDati.modelObjects;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CittadinoTest extends Cittadino {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
