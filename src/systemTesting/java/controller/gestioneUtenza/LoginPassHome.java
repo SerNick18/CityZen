9caa313f9eb2b769f3fcbef7d165e620f547fc4d
leo-capvano <leocapuano0@gmail.com>
2021-01-18 10:03:50 +0100

    - fix test

--- src/systemTesting/java/controller/gestioneUtenza/LoginPassHome.java
+++ src/systemTesting/java/controller/gestioneUtenza/LoginPassHome.java
@@ -2,6 +2,9 @@ package controller.gestioneUtenza;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
+import model.gestioneDati.facadeDataAccess.FacadeDAO;
+import model.gestioneDati.modelObjects.Cittadino;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -32,6 +35,8 @@ public class LoginPassHome {
     private String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
+    FacadeDAO service = new FacadeDAO();
+    Cittadino cittadino;
 
     @Before
     public void setUp() throws Exception {
@@ -39,6 +44,9 @@ public class LoginPassHome {
         driver = new FirefoxDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        cittadino = new Cittadino("SBAFNC98T26H703S","Francesco","Sabia","Password1!",
+                "via europa",12,"Salerno","abcd98@gmail.com",0,0);
+        service.registraCittadino(cittadino);
     }
 
     @Test
@@ -46,10 +54,10 @@ public class LoginPassHome {
         driver.get("http://localhost:8080/CityZen_war_exploded/login.jsp");
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).clear();
-        driver.findElement(By.id("email")).sendKeys("leocapuano0@gmail.com");
+        driver.findElement(By.id("email")).sendKeys(cittadino.getEmail());
         driver.findElement(By.id("pwd")).click();
         driver.findElement(By.id("pwd")).clear();
-        driver.findElement(By.id("pwd")).sendKeys("Password1!");
+        driver.findElement(By.id("pwd")).sendKeys(cittadino.getPwd());
         driver.findElement(By.xpath("//body")).click();
         driver.findElement(By.id("loginId")).click();
         driver.findElement(By.linkText("Home")).click();
@@ -57,6 +65,7 @@ public class LoginPassHome {
 
     @After
     public void tearDown() throws Exception {
+        service.eliminaCittadino(cittadino.getCF());
         driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
